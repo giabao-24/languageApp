9fe2d28151859c1ae2a87e1ba66f3a1311148c8d
giabao-24 <baocaohuy123@gmail.com>
2026-09-09 22:27:35 +0700

    Fix createVocabQuiz API bugs

--- controller/VocabQuizController.js
+++ controller/VocabQuizController.js
@@ -2,13 +2,16 @@ const VocabQuiz = require('../models/VocabQuiz');
 
 const createVocabQuiz = async (req,res) => {
   const {topicName,level,words} = req.body;
+  if(!topicName || !level || !words) {
+    return res.status(400).json({message:'Vui lòng nhập đầy đủ Chủ đề, Cấp độ và ít nhất 1 từ vựng!'});
+  }
   try {
-     await VocabQuiz.create({
+     const newVocab = await VocabQuiz.create({
       topicName,
       level,
       words
      })
-     res.status(201).json({message:'Tạo thành công!'});
+     res.status(201).json({message:'Tạo thành công!',newVocab});
   }catch(error) {
      res.status(500).json({message:'Lỗi khi tạo!',error: error.message});
   }
